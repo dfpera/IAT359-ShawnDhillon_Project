a3c8437ac7b7bd0a48348e3b7007bec6e6de53d2
Shawn Dhillon <shawnd@sfu.ca>
2017-11-05 15:35:50 -0800

    Implement Database.

---
+++ app/src/main/java/com/shawn/mapapp/database/Constants.java
@@ -0,0 +1,18 @@
+package com.shawn.mapapp.database;
+
+/**
+ * Created by shawndhillon on 2017-11-05.
+ */
+
+public class Constants {
+    public static final String DATABASE_NAME = "MY_DATABASE";
+    public static final String TABLE_NAME = "LOCATION_TABLE";
+    public static final String UID = "_id";
+    public static final String NAME = "MARKER_NAME";
+    public static final String LONG = "LONGITUTDE";
+    public static final String LAT = "LATITUDE";
+    public static final String Z_INDEX = "Z_INDEX";
+    public static final String USERNAME = "USERNAME";
+    public static final String IMG_PATH = "IMG_PATH";
+    public static final int DATABASE_VERSION = 1;
+}
---
+++ app/src/main/java/com/shawn/mapapp/database/MyDatabase.java
@@ -0,0 +1,63 @@
+package com.shawn.mapapp.database;
+
+/**
+ * Created by shawndhillon on 2017-11-05.
+ */
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+public class MyDatabase {
+    private SQLiteDatabase db;
+    private Context context;
+    private final MyHelper helper;
+
+    public MyDatabase (Context c){
+        context = c;
+        helper = new MyHelper(context);
+    }
+
+    public long insertData (String name, double longitude, double latitude, float zIndex, String username, String imgPath)
+    {
+        db = helper.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(Constants.NAME, name);
+        contentValues.put(Constants.LONG, longitude);
+        contentValues.put(Constants.LAT, latitude);
+        contentValues.put(Constants.Z_INDEX, zIndex);
+        contentValues.put(Constants.USERNAME, username);
+        contentValues.put(Constants.IMG_PATH, imgPath);
+        long id = db.insert(Constants.TABLE_NAME, null, contentValues);
+        return id;
+    }
+
+    public Cursor getData(String username)
+    {
+        // Select the markers that are owned by a username
+        SQLiteDatabase db = helper.getWritableDatabase();
+        // Returns these columns in the query
+        String[] columns = {Constants.NAME, Constants.LONG, Constants.LAT, Constants.Z_INDEX, Constants.IMG_PATH};
+
+        String selection = Constants.USERNAME + "='" + username + "'";  //Constants.USERNAME = 'username'
+        Cursor cursor = db.query(Constants.TABLE_NAME, columns, selection, null, null, null, null);
+
+        return cursor;
+    }
+
+    // TODO: Search db based on name of location
+    public Cursor getSelectedData(String username, String name)
+    {
+        // Select the markers that are owned by a username
+        SQLiteDatabase db = helper.getWritableDatabase();
+        // Returns these columns in the query
+        String[] columns = {Constants.NAME, Constants.LONG, Constants.LAT, Constants.Z_INDEX, Constants.IMG_PATH};
+
+        String selection = Constants.USERNAME + "='" + username + "'";  //Constants.USERNAME = 'username'
+        Cursor cursor = db.query(Constants.TABLE_NAME, columns, selection, null, null, null, null);
+
+        return cursor;
+    }
+
+}
